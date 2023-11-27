31b58be647bf502517d80db595f1253f3edc5ec7
Cristian Antonio Ramos Ludueña <r.cristian2704@gmail.com>
2023-11-27 11:49:09 -0300

    Navbar inicio home

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import '../assets/css/navbar.css'
+import { HiOutlineBars3 } from "react-icons/hi2"
+import {
+    Box, 
+    Drawer, 
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText
+} from "@mui/material";
+import Home from "@mui/icons-material/Home";
+import Info from "@mui/icons-material/Info";
+import Telefono from "@mui/icons-material/PhoneRounded";
+import Carrito from "@mui/icons-material/ShoppingCartRounded";
+
+const Navbar = () => {
+
+    // Declaro si se abre o no el menú
+    const [abrirMenu, setAbrirMenu] = useState(false); 
+    // Opciones que tiene la navbar
+    const menuOpciones = [
+        {
+            texto: "Inicio",
+            icono: <Home/>
+        },
+        {
+            texto: "Ranking",
+            icono: <Info/>
+        },
+        {
+            texto: "Galería",
+            icono: <Info/>
+        },
+        {
+            texto: "Servicios",
+            icono: <Info/>
+        },
+        {
+            texto: "Suscripción",
+            icono: <Telefono/>
+        },
+        {
+            texto: "Carrito",
+            icono: <Carrito/>
+        }
+    ]
+
+    return <nav>
+        <div className="navbar-logo-container">
+            PICA PONCHO
+        </div>
+        <div className="navbar-links-container">
+            <a href="#">Inicio</a>
+            <a href="#">Ranking</a>
+            <a href="#">Galería</a>
+            <a href="#">Servicios</a>
+            <a href="#">Suscribirse</a>
+        </div>
+        <div className="navbar-menu-container">
+            <HiOutlineBars3 onClick={() => setAbrirMenu(true)} />
+        </div>
+        <Drawer open={ abrirMenu } onClose={ () => setAbrirMenu(false) } anchor="right">
+            <Box 
+                sx={{width: 250}}
+                role="presentation"
+                onClick={() => setAbrirMenu(false)}
+                onKeyDown={() => setAbrirMenu(false)}>
+                    <List>
+                        { menuOpciones.map((item) => (
+                            <ListItem key={item.texto} disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>{item.icono}</ListItemIcon>
+                                    <ListItemText primary={item.texto}/>
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+            </Box>
+        </Drawer>
+    </nav>
+}
+
+export default Navbar;
